0622845e440b84fd5f369a446759ba98d378d1b6
Grecia-Torres <grecia.atorres@gmail.com>
2024-09-25 11:23:47 -0700

    completed ShoppingList app

--- workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "Milk " + "Juice " + "Yogurt " + "Bread " + "Jam " + "Creamer " + "Coffee " + "Sauce " + "Ketchup " + "Hashbrowns " + "Chicken Broth"
+        );
+
 }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
