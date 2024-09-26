0cbe92513c2834ee72858ec632522859de843817
Grecia-Torres <grecia.atorres@gmail.com>
2024-09-26 15:36:27 -0700

    Completed Mod3 - Exercise 4 - Payroll Calculator

---
+++ workbook-1/BasicCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String employeeName = scanner.nextLine();
+
+        System.out.print("How many hours have you worked? ");
+        float hoursWorked = scanner.nextInt();
+
+        System.out.print("What is your pay rate? ");
+        float payRate = scanner.nextInt();
+
+        float grossPay = hoursWorked * payRate;
+
+        System.out.println(employeeName + "'s" + " Gross Pay is " + grossPay );
+    }
+}
