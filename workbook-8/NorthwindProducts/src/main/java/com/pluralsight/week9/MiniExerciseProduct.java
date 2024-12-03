55ffc4c75d45ff8db5b2f204d31695168d8eb9c3
Grecia-Torres <grecia.atorres@gmail.com>
2024-12-03 10:35:36 -0800

    fixes

--- workbook-8/NorthwindProducts/src/main/java/com/pluralsight/week9/MiniExerciseProduct.java
+++ workbook-8/NorthwindProducts/src/main/java/com/pluralsight/week9/MiniExerciseProduct.java
@@ -1,4 +1,27 @@
 package com.pluralsight.week9;
 
+
+import java.sql.*;
+
 public class MiniExerciseProduct {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "");
+
+    }
+    public static void displayAllCars {
+
+        String query = "SELECT ProductName, ProductID FROM Products ";
+        PreparedStatement statement = connection.prepareStatement(query);
+
+
+        ResultSet resultSet = statement.executeQuery(query);
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("ProductName"));
+        }
+
+        connection.close();
+    }
+}
 }
